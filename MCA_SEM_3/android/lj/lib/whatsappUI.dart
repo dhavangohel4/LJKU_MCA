49e24f637674ec3b42560bbc27203a1f1d06ce46
Your Name <dhavangohel09@gmail.com>
2026-08-04 14:05:01 +0530

    List View celtur2 4/8

--- MCA_SEM_3/android/lj/lib/whatsappUI.dart
+++ MCA_SEM_3/android/lj/lib/whatsappUI.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
 
 class whatsappUI extends StatelessWidget {
-  const whatsappUI({super.key});
-
+  whatsappUI({super.key});
+  List color = [Colors.red,Colors.blue,Colors.green,Colors.greenAccent,Colors.cyan];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text(
+          "WHATSAPP",
+          style:TextStyle(
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+        ),
+        ),
+        actions: [
+          Icon(
+            Icons.camera_alt_outlined,
+            color: Colors.white,
+          ),
+          SizedBox(width: 20),
+          Icon(
+            Icons.search,
+            color: Colors.white,
+          ),
+          SizedBox(width: 20),
+          Icon(
+            Icons.more_vert,
+            color: Colors.white,
+          ),
+        ],
+      ),
+      body: ListView.builder(
+          itemCount: color.length,
+          itemBuilder: (context,index){
+            return Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  color: color[index],
+                  // ?Colors.blue,
+                  child: Text("$index"),
+                ),
+              ),
+            );
 
+          }),
     );
   }
 }
