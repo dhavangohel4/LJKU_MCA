e20077df2e39b43c47eb7659cc2be582ea271c33
Your Name <dhavangohel09@gmail.com>
2026-09-11 12:36:17 +0530

    Update Flutter project12:36

--- MCA_SEM_3/android/lj/lib/main.dart
+++ MCA_SEM_3/android/lj/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'login/login.dart';
 import 'snackbar_navigationbar.dart';
 import 'splash_screen.dart';
 
@@ -34,7 +35,7 @@ class MyApp extends StatelessWidget {
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      home: snackbar_navigationbar(),
+      home: LoanTopupScreen(),
       // home: MyApp(),
     );
   }
